ced3fe278123ef97765934925e8ff86fa26644ed
Carlos Mayorga <carlos.mayorga@realplaza.com.pe>
2025-09-03 10:33:06 -0500

    feat(trackings): inicializar proyecto Spring Boot
    - Configura la estructura base del proyecto utilizando Spring Boot
    - Agrega configuración inicial en `application.properties`
    - Integra dependencias necesarias en el archivo `pom.xml`
    - Incluye Docker Compose para gestionar PostgreSQL
    - Añade scripts de Maven Wrapper

---
+++ src/main/java/com/example/trackings/TrackingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trackings/TrackingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
